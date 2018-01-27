1caeaac6fb8bbe47a642d125246e06a7daa57a15
denmgarcia <dengarcia.x@gmail.com>
2018-01-27 21:38:23 +0800

    Added files

--- apple/accounts/views.py
+++ apple/accounts/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'accounts/login.html')
+    numbers = [1,2,3,4]
+    name = "Denmark Garcia"
+    args = {
+       'myname': "Denmark Garcia",
+       'course':'Computer Science'
+
+   }
+    return render(request,'accounts/home.html',args)
